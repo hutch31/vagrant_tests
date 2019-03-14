fcb2cd66756044cae718fd4ac5e13804252e9f40
Guy Hutchison <ghutchis@gmail.com>
2019-03-14 13:38:18 -0700

    Added setup script for balsa tools

---
+++ vagrant_yosys/balsa.sh
@@ -0,0 +1,45 @@
+
+sudo apt-get update
+sudo apt-get install -y guile-1.8 libglib2.0-dev libgtk2.0-dev
+
+cd /vagrant
+if [ ! -f balsa-4.0.tar.gz ]; then
+wget http://apt.cs.manchester.ac.uk/ftp/pub/apt/balsa/4.0/balsa-4.0.tar.gz
+wget http://apt.cs.manchester.ac.uk/ftp/pub/apt/balsa/4.0/balsa-sim-verilog-4.0.tar.gz
+wget http://apt.cs.manchester.ac.uk/ftp/pub/apt/balsa/4.0/balsa-tech-example-4.0.tar.gz
+wget http://apt.cs.manchester.ac.uk/ftp/pub/apt/balsa/4.0/balsa-tech-minimal-4.0.tar.gz
+fi
+
+cd
+if [ -f balsa-4.0 ]; then
+tar xzf /vagrant/balsa-4.0.tar.gz
+tar xzf /vagrant/balsa-sim-verilog-4.0.tar.gz
+tar xzf /vagrant/balsa-tech-example-4.0.tar.gz
+tar xzf /vagrant/balsa-tech-minimal-4.0.tar.gz
+fi
+
+cd balsa-4.0
+./configure
+make
+sudo make install
+cd ..
+
+cd balsa-sim-verilog-4.0
+./configure
+make
+sudo make install
+cd ..
+
+cd balsa-tech-example-4.0
+./configure
+make
+sudo make install
+cd ..
+
+cd balsa-tech-minimal-4.0
+./configure
+make
+sudo make install
+cd ..
+
+
